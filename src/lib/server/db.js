46d0a0aec897d0ea6c0374c22513daeb8a87fa20
Toy085 <brightabaka1@gmail.com>
2025-12-18 16:55:26 +0100

    Move database client code to server directory and re-establish database connection

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,4 +1,3 @@
-// src/lib/db/client.js
 import Database from 'better-sqlite3';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -6,9 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Point to your database file
 const dbPath = path.join(__dirname, 'library.db');
 
-console.log('DB path:', dbPath);
-
-// Export DB instance
 export const db = new Database(dbPath);
